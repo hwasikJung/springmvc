835c419f6f3ddd2908e68c4087f47d10a803b0b8
hs.jung <hs.jung@sqisoft.com>
2022-05-31 17:10:17 +0900

    init

--- src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
@@ -1,4 +1,4 @@
-package hello.springmvc.baisc.requestmapping;
+package hello.springmvc.basic.requestmapping;
 
 
 import org.springframework.web.bind.annotation.*;
--- src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -1,4 +1,4 @@
-package hello.springmvc.baisc.requestmapping;
+package hello.springmvc.basic.requestmapping;
 
 
 import org.slf4j.Logger;
